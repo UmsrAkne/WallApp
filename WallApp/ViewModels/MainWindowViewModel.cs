5d080124d4d120484b3993e3c021c917764b9073
Akn <sibata486@gmail.com>
2023-02-23 00:02:44 +0900

    init

---
+++ WallApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Mvvm;
+
+namespace WallApp.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "Prism Application";
+
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
