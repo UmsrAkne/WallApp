0fc49491407dc4d1cdf90d84bb77a509769b1db2
Akn <sibata486@gmail.com>
2023-02-23 00:24:15 +0900

    add / MainWindowViewModel のプロパティをビューとバインディング

--- WallApp/ViewModels/MainWindowViewModel.cs
+++ WallApp/ViewModels/MainWindowViewModel.cs
@@ -7,10 +7,26 @@ namespace WallApp.ViewModels
     {
         private string title = "Prism Application";
 
+        private double width = 600;
+        private double height = 480;
+        private double posX;
+        private double posY;
+        private bool isTopMost = true;
+
         public string Title
         {
             get { return title; }
             set { SetProperty(ref title, value); }
         }
+
+        public double Width { get => width; set => SetProperty(ref width, value); }
+
+        public double Height { get => height; set => SetProperty(ref height, value); }
+
+        public double PosX { get => posX; set => SetProperty(ref posX, value); }
+
+        public double PosY { get => posY; set => SetProperty(ref posY, value); }
+
+        public bool IsTopMost { get => isTopMost; set => SetProperty(ref isTopMost, value); }
     }
 }
