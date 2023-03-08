ad7afc4170df771690c0044f100ed639794b4b7a
Akn <sibata486@gmail.com>
2023-03-09 00:04:35 +0900

    del feat / ウィンドウの位置やサイズを数値入力する機能を削除
    細かい数値を入力する機能は不要だったので削除。

--- WallApp/ViewModels/MainWindowViewModel.cs
+++ WallApp/ViewModels/MainWindowViewModel.cs
@@ -10,10 +10,6 @@ namespace WallApp.ViewModels
     {
         private string title = "Prism Application";
 
-        private double width = 600;
-        private double height = 480;
-        private double posX;
-        private double posY;
         private bool isTopMost = true;
         private int beforeVolume;
         private bool mute;
@@ -27,62 +23,6 @@ namespace WallApp.ViewModels
         {
             get { return title; }
             private set { SetProperty(ref title, value); }
-        }
-
-        public double Width
-        {
-            get => width;
-            set
-            {
-                if (Application.Current.MainWindow != null)
-                {
-                    Application.Current.MainWindow.Width = value;
-                }
-
-                SetProperty(ref width, value);
-            }
-        }
-
-        public double Height
-        {
-            get => height;
-            set
-            {
-                if (Application.Current.MainWindow != null)
-                {
-                    Application.Current.MainWindow.Height = value;
-                }
-
-                SetProperty(ref height, value);
-            }
-        }
-
-        public double PosX
-        {
-            get => posX;
-            set
-            {
-                if (Application.Current.MainWindow != null)
-                {
-                    Application.Current.MainWindow.Left = value;
-                }
-
-                SetProperty(ref posX, value);
-            }
-        }
-
-        public double PosY
-        {
-            get => posY;
-            set
-            {
-                if (Application.Current.MainWindow != null)
-                {
-                    Application.Current.MainWindow.Top = value;
-                }
-
-                SetProperty(ref posY, value);
-            }
         }
 
         public bool IsTopMost
